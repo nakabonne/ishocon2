9abd2d4c47ac75cfd735d9e23e97f91044e38129
nakabonne <rodriguez.nak@gmail.com>
2018-08-25 11:07:44 +0900

    goのMakefile作成

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -4,7 +4,7 @@ cd /home/ishocon/webapp
 git pull
 sudo cp etc/nginx.conf /etc/nginx/nginx.conf
 cd /home/ishocon/webapp/go
-#make TODO: ビルド
+make build
 sudo /usr/sbin/nginx -t
 sudo service nginx reload
 #sudo service isubata.golang restart TODO: リスタート
