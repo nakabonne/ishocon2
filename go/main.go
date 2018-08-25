24f40c24073aca0aad81da1acb01681fdf9a0fd4
nakabonne <rodriguez.nak@gmail.com>
2018-08-25 17:51:51 +0900

    fi

--- go/main.go
+++ go/main.go
@@ -176,7 +176,7 @@ func main() {
 		var message string
 		r.SetHTMLTemplate(template.Must(template.ParseFiles(layout, "templates/vote.tmpl")))
 		// TODO: これでだめだったら、keyを全フィールド合わせたstringにする
-		if !userExist || user.Name != c.PostForm("name") || user.Address != c.PostForm("address") {
+		if !userExist {
 			message = "個人情報に誤りがあります"
 		} else if user.Votes < voteCount+votedCount {
 			message = "投票数が上限を超えています"
